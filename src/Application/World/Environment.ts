018c8a94c15d2a9abf01327f356dc3ca66fd3505
조병근 <airmancho@korea.ac.kr>
2022-12-13 20:52:54 +0900

    remove debugger

--- src/Application/World/Environment.ts
+++ src/Application/World/Environment.ts
@@ -1,34 +1,14 @@
 import * as THREE from "three";
-import GUI from "lil-gui";
 import { BaseObject } from "./BaseObject.js";
 
 export default class Environment extends BaseObject {
-  gui: GUI = new GUI();
   sunLight: THREE.DirectionalLight;
   ambientLight: THREE.AmbientLight;
-  obj: { [key: string]: any };
 
   constructor() {
     super();
 
-    // this.gui = new GUI({ container: document.querySelector(".hero-main") });
-    this.obj = {
-      colorObj: { r: 0, g: 0, b: 0 },
-      intensity: 3,
-    };
-
     this.setSunlight();
-  }
-
-  setGUI() {
-    this.gui.addColor(this.obj, "colorObj").onChange(() => {
-      this.sunLight.color.copy(this.obj.colorObj);
-      this.ambientLight.color.copy(this.obj.colorObj);
-    });
-    this.gui.add(this.obj, "intensity", 0, 10).onChange(() => {
-      this.sunLight.intensity = this.obj.intensity;
-      // this.sunLight.ambientLight = this.obj.intensity;
-    });
   }
 
   setSunlight() {
@@ -37,8 +17,6 @@ export default class Environment extends BaseObject {
     this.sunLight.shadow.camera.far = 20;
     this.sunLight.shadow.mapSize.set(2048, 2048);
     this.sunLight.shadow.normalBias = 0.05;
-    // const helper = new THREE.CameraHelper(this.sunLight.shadow.camera);
-    // this.scene.add(helper);
 
     this.sunLight.position.set(-1.5, 7, 3);
     this.scene.add(this.sunLight);
